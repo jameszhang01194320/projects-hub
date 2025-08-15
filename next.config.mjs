026816c73acccf1d165c51c3cecaff26d166793a
jameszhang01194320 <wuab@yahoo.com>
2025-08-14 21:34:45 -0700

    projects hub init

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
+// next.config.js
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+const nextConfig = {
+  async rewrites() {
+    return [
+      { source: '/portfolio/:path*', destination: 'https://my-portfolio-bootstrap-sigma.vercel.app/:path*' },
+      { source: '/note/:path*',   destination: 'https://sharecode-frontend.vercel.app/:path*' },
+      { source: '/repair/:path*', destination: 'https://frontend-repair.vercel.app/:path*' },
+    ];
+  },
+};
 export default nextConfig;
